12ccf27694fb78da1826676042c9a9e000d80260
PALURU JASWANTH <155042203+palurujaswanth@users.noreply.github.com>
2026-04-24 15:14:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-// Typing Effect
-const text = "Hi, I'm Jaswanth 👋";
-let i = 0;
-
-function typing() {
-    if (i < text.length) {
-        document.getElementById("typing").innerHTML += text.charAt(i);
-        i++;
-        setTimeout(typing, 80);
-    }
-}
-typing();
-
-// Form Message
-function sendMsg() {
-    document.getElementById("msg").innerText = "Message Sent Successfully!";
+function sendMsg(){
+    document.getElementById("msg").innerText = "Message sent successfully!";
     return false;
 }
